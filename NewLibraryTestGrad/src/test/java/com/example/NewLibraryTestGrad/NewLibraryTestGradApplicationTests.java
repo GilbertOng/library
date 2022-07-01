9311e92ee579d6900165e63cf4715569300026e8
GilbertOng <83915894+GilbertOng@users.noreply.github.com>
2022-07-01 09:17:22 +0700

    Add files via upload

---
+++ NewLibraryTestGrad/src/test/java/com/example/NewLibraryTestGrad/NewLibraryTestGradApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NewLibraryTestGrad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewLibraryTestGradApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
